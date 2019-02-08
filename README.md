73f50c5d3d12e720f151e17699cc2c051f9e879b
Labin Tamang <labintamang@Labins-MacBook-Pro.local>
2019-02-08 15:54:38 -0800

    Addresed review comments

--- README.md
+++ README.md
@@ -1,16 +1,5 @@
-# Pixel Art Maker Project
+# Pixel Art Maker
+This is a pixel art maker where user can choose height and weight to create a grid. User can choose their own color to fill the cell in the grid.
 
-## Table of Contents
-
-* [Instructions](#instructions)
-* [Contributing](#contributing)
-
-## Instructions
-
-To get started, open `designs.js` and start building out the app's functionality.
-
-For specific, detailed instructions, look at the project instructions in the [Udacity Classroom](https://classroom.udacity.com/me).
-
-## Contributing
-
-This repository is the starter code for _all_ Udacity students. Therefore, we most likely will not accept pull requests.
+#Instructions
+Height and width of the grid should be between 1 to 20 inclusive.
--- designs.js
+++ designs.js
@@ -1,33 +1,37 @@
-
-let canvas= document.getElementById('pixelCanvas');
+let canvas = document.getElementById('pixelCanvas');
 let colorPicker = document.getElementById("colorPicker");
 let sizePicker = document.getElementById("sizePicker");
 
-let inputHeight= document.getElementById('inputHeight').value;
-let inputWidth= document.getElementById('inputWidth').value;
+let inputHeight = document.getElementById('inputHeight').value;
+let inputWidth = document.getElementById('inputWidth').value;
 
 makeGrid(inputHeight, inputWidth);
 
-sizePicker.addEventListener("click", (e) => {
-  e.preventDefault();
+sizePicker.addEventListener("submit", (e) => {
+    e.preventDefault();
 
-  let inputHeight = document.getElementById("inputHeight").value;
-  let inputWidth = document.getElementById("inputWidth").value;
+    inputHeight = document.getElementById("inputHeight").value;
+    inputWidth = document.getElementById("inputWidth").value;
 
-  canvas.firstElementChild.remove();
+    canvas.firstElementChild.remove();
 
-  makeGrid(inputHeight, inputWidth);
+    makeGrid(inputHeight, inputWidth);
 
 });
 
+/**
+* @description Provided height and width, creates grid.
+* @param {number} height - The height of the grid
+* @param {number} width - The width of the gri
+*/
 function makeGrid(height, width) {
-  for (let i = 0; i < height; i++) {
-    let row = canvas.insertRow(i);
-    for (let j = 0; j < width; j++) {
-      let cell = row.insertCell(j);
-      cell.addEventListener("click", (e) => {
-                            cell.style.backgroundColor = colorPicker.value;
-                           })
+    for (let i = 0; i < height; i++) {
+        let row = canvas.insertRow(i);
+        for (let j = 0; j < width; j++) {
+            let cell = row.insertCell(j);
+            cell.addEventListener("click", (e) => {
+                cell.style.backgroundColor = colorPicker.value;
+            })
+        }
     }
-  }
 }
